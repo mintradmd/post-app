db1672736e250f8fe8b9d5597d169df564d61cf6
NidhiSharmaSignavio <nidhi.sharma@signavio.com>
2021-11-30 11:01:45 +0100

    Update App.js - add Posts
    Test passed: displays posts on load. Failing acceptance: Display Title

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App:',()=>{
+  beforeEach(()=>{
+    render(<App/>);
+  })
+  
+  test('Displays posts on load', () => {
+    expect(screen.queryAllByTestId('post-title').length).toBeGreaterThan(0);
+  });
+
+  test('Display Title', () => {
+    expect(screen.queryByText('sunt aut facere repellat provident occaecati excepturi optio reprehenderit')).toBeInTheDocument();
+  });
+})
+

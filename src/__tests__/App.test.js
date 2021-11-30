5f30aa762c79553f8a64c2afdf354ac62a1adc8a
NidhiSharmaSignavio <nidhi.sharma@signavio.com>
2021-11-30 14:54:54 +0100

    Pass test App.test.js
    After mmocking

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,17 +1,67 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen, waitFor } from '@testing-library/react';
+import Posts from '../Posts';
+import * as networkCalls from '../networkCalls';
+
+const posts=[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "nesciunt quas odio",
+    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+  },
+]
+
 
 describe('App:',()=>{
   beforeEach(()=>{
-    render(<App/>);
-  })
+      const mockGetPosts = jest.spyOn(networkCalls,'getPosts');
+      mockGetPosts.mockResolvedValue(posts);
+      render(<Posts/>)  
+    })
+
+    afterEach(()=>{
+      jest.restoreAllMocks();
+    })
+
+    
+
   
-  test('Displays posts on load', () => {
+  test('Displays posts on load', async() => {
+   await waitFor(()=>{
     expect(screen.queryAllByTestId('post-title').length).toBeGreaterThan(0);
+   })
+    
   });
 
-  test('Display Title', () => {
-    expect(screen.queryByText('sunt aut facere repellat provident occaecati excepturi optio reprehenderit')).toBeInTheDocument();
+  test('Display Title', async() => {
+    await waitFor(()=>{
+      expect(screen.queryByText('sunt aut facere repellat provident occaecati excepturi optio reprehenderit')).toBeInTheDocument();
+    })
+   
   });
 })
 
--- src/__tests__/Posts.test.js
+++ src/__tests__/Posts.test.js
@@ -1,31 +1,60 @@
 import Posts from '../Posts';
-import {screen, render} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import * as networkCalls from '../networkCalls';
+
+const posts=[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "nesciunt quas odio",
+    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+  },
+]
 
 describe('Posts:',()=>{
-    beforeEach(()=>{
-        const posts=[
-            {
-              "userId": 1,
-              "id": 2,
-              "title": "qui est esse",
-              "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-            },
-            {
-              "userId": 1,
-              "id": 3,
-              "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-              "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-            },
-          ]
-        render(<Posts posts={posts}/>);
-    })
 
-    test('Should Display Comments on Click', () => {
+  beforeEach(()=>{
+    const mockGetPosts = jest.spyOn(networkCalls,'getPosts');
+    mockGetPosts.mockResolvedValue(posts);
+    render(<Posts/>)  
+  })
+
+  afterEach(()=>{
+    jest.restoreAllMocks();
+  })
+
+    
+    test('Should Display Comments on Click', async() => {
         const postContainer = screen.queryAllByTestId('post-title');
         const firstPost = postContainer[0]
         firstPost.click();
-        
-        expect(screen.queryByText('laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium')).toBeInTheDocument();
+        await waitFor(()=>{
+          expect(screen.queryByText('laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium')).toBeInTheDocument();
+        })
     })
 })
 
